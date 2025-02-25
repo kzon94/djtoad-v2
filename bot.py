719531ab536f51583cf1255ddc97a6b5d9cecd5f
Victor Belinchón <vbelinchonperez@gmail.com>
2025-02-25 14:29:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 
 import os
+import asyncio  # Importar asyncio
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
@@ -14,18 +15,23 @@ intents = discord.Intents.default()
 intents.message_content = True  # Necesario para recibir el contenido de los mensajes
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Cargar los módulos de comandos
-from commands.music_commands import MusicCommands
-from commands.other_commands import OtherCommands
-
-# Añadir los comandos al bot
-bot.add_cog(MusicCommands(bot))
-bot.add_cog(OtherCommands(bot))
-
 # Evento cuando el bot está listo
 @bot.event
 async def on_ready():
     print(f'✅ Tu rana favorita conectada como {bot.user}')
 
+# Función asíncrona para configurar el bot
+async def setup_bot():
+    # Cargar los módulos de comandos
+    from commands.music_commands import MusicCommands
+    from commands.other_commands import OtherCommands
+
+    # Añadir los comandos al bot
+    await bot.add_cog(MusicCommands(bot))
+    await bot.add_cog(OtherCommands(bot))
+
+    # Iniciar el bot
+    await bot.start(DISCORD_TOKEN)
+
 # Ejecutar el bot
-bot.run(DISCORD_TOKEN)
+asyncio.run(setup_bot())
